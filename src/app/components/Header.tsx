5d1bc9d59c8a04fcd3b79f71aade8d8e29134c32
Emmy Nguyen <145530039+tastytough@users.noreply.github.com>
2025-10-20 14:26:39 -0700

    Remove unnecessary code

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -90,9 +90,9 @@ export const Header = () => {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               className={`lucide lucide-arrow-right-icon lucide-arrow-right ${
                 hovering
                   ? "opacity-100 translate-x-0"
