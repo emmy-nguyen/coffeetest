3ec4268d76a966914fd398ea6d3ee3008e226999
Emmy Nguyen <145530039+tastytough@users.noreply.github.com>
2025-10-20 12:05:48 -0700

    Initial commit and add Header component

---
+++ src/app/components/MacButton.tsx
@@ -0,0 +1,13 @@
+import { ReactElement } from "react";
+
+export const MacButton = ({
+  icon,
+  text,
+  hoverIcon,
+}: {
+  icon: ReactElement;
+  text: string;
+  hoverIcon: ReactElement;
+}) => {
+  return <div>Button</div>;
+};
